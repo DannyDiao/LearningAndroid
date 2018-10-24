1760ce74aac43a7619182930eedbef848e1ed710
DannyDiao <723714386@qq.com>
2018-10-24 22:21:25 +0800

    Update Codes
    Update Codes on 10.24

--- MaterialTest/app/src/main/java/com/dannydiao/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/dannydiao/materialtest/MainActivity.java
@@ -6,26 +6,93 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity  {
 
+
     private DrawerLayout mDrawerLayout;
 
+    private SwipeRefreshLayout swipeRefresh;
+
+    private Fruit[] fruits = {new Fruit("Apple",R.drawable.apple),
+                              new Fruit("Banana",R.drawable.banana),
+            new Fruit("橙子",R.drawable.orange),
+            new Fruit("梨",R.drawable.pear),
+            new Fruit("菠萝",R.drawable.pineapple),
+            new Fruit("樱桃",R.drawable.cherry),
+            new Fruit("西瓜",R.drawable.watermelon),
+            new Fruit("葡萄",R.drawable.grape),
+            new Fruit("草莓",R.drawable.strawberry),
+            new Fruit("芒果",R.drawable.mango)};
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
+
+    private void initFruits(){
+        fruitList.clear();
+        for ( int i = 0;i < 50; i++){
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+
+        }
+    }
+
+    private void refreshFruits(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
@@ -36,6 +103,12 @@ public class MainActivity extends AppCompatActivity  {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
         FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
 
 
 
