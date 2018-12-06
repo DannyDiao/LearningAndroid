0b47fdd6e6e309561b38f665c246f923c5eee248
DannyDiao <723714386@qq.com>
2018-12-06 21:10:13 +0800

    又捡起了《第一行代码》
    因为最近想用NavigationMenu重构快行，所以又捡起了第一行代码撸一下fragment。准备期末了不知道每天还能不能保证代码量。另外安卓进阶之光的确一般般...现在换安卓群英传看

---
+++ Android5_Test/app/src/main/java/com/dannydiao/android5_test/HomeAdapter.java
@@ -0,0 +1,6 @@
+package com.dannydiao.android5_test;
+
+import android.support.v7.widget.RecyclerView;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter> {
+}
--- Android5_Test/app/src/main/java/com/dannydiao/android5_test/MainActivity.java
+++ Android5_Test/app/src/main/java/com/dannydiao/android5_test/MainActivity.java
@@ -1,7 +1,13 @@
 package com.dannydiao.android5_test;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView mRecyclerView = (RecyclerView) this.findViewById(R.id.recycleview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
     }
 }
+
+
